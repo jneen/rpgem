fc789e1453867a5103addd3254769c7b8ebd2334
Jay Adkisson <jay@causes.com>
2010-05-16 15:46:26 -0700

    vanilla fetch, spec, and build work, plus smart versions

--- lib/rpgem.rb
+++ lib/rpgem.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 require 'rubygems'
 require 'rubygems/format'
+require 'erb'
 
 module FileUtils
   def self.in_dir(dir)
@@ -26,23 +27,36 @@ class Hash
   end
 end
 
+class << File
+  def write(fname, str)
+    self.open(fname, "w") do |f|
+      f.write str
+    end
+  end
+end
+
+LIB_DIR = File.expand_path(File.dirname(__FILE__))
+
 
 class RPGem
+  HOME_DIR = `echo ~`.strip
   CWD = `pwd`.strip
-  TMP = "#{CWD}/tmp"
-  BUILD = "#{CWD}/rpmbuild"
+  TMP = "/tmp/rpgem"
+  RPMBUILD_DIR = "#{HOME_DIR}/rpmbuild"
+  SOURCES_DIR = "#{RPMBUILD_DIR}/SOURCES"
+  SPECS_DIR = "#{RPMBUILD_DIR}/SPECS"
 
   def initialize(name, options={})
+    @name = name
     options.rmerge!({
       :version => nil,
       :version_reqs => [],
       :recursive => false
+      :ruby_cmd => "ruby"
     })
-
-    @name = name
-    @version_reqs = options[:version_reqs]
-    @version = options[:version]
-    @recursive = options[:recursive]
+    options.each do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
   end
 
   def sh_version_reqs
@@ -61,46 +75,47 @@ class RPGem
     end
   end
 
-  def sources_dir
-    "#{BUILD}/SOURCES"
+  def gem_loc
+    "#{SOURCES_DIR}/#{self.to_s}.gem"
   end
 
-  def gem_loc
-    "#{sources_dir}/#{self.to_s}.gem"
+  def spec_loc
+    "#{SPECS_DIR}/rpgem-#{self.to_s}.gem"
   end
 
   def format
-    Gem::Format.from_file_by_path(gem_loc)
+    @format ||= Gem::Format.from_file_by_path(gem_loc)
   end
 
   def spec
-    format.spec
+    @spec ||= format.spec
   end
 
-  def req_satisfied?(name_or_dep, requirements=nil)
-    current = `ls #{sources_dir} | grep '^#{d.name}' | head -1`
-    return false if current.empty?
-
+  def fetched_version(name_or_dep, requirements=[])
     if name_or_dep.is_a? Gem::Dependency
       d = name_or_dep
     else
       d = Gem::Dependency.new(name_or_dep, requirements)
     end
-    d.match?(d.name, current.match(/-(\d+(\.\d+)+)\.gem$/)[1])
+
+    currents = `ls #{SOURCES_DIR} | grep '^#{d.name}'`.split($/)
+    return false if currents.empty?
+
+    currents.each do |c|
+      ver = c.match(/-(\d+(\.\d+)+)\.gem$/)[1]
+      return ver if d.match?(d.name, ver)
+    end
+    return nil
   end
 
   def fetched?
-    if @version
-      return File.exists? self.gem_loc
-    else if !@version_reqs.empty?
-      return @version_reqs.all? do |r|
-        req_satisfied? @name, r
-      end
-    end
+    return File.exists?(self.gem_loc)
   end
 
   def fetch!
-    if self.fetched?
+    v = self.fetched_version(@name, @version_reqs)
+    if (@version.nil? && v) || (!@version.nil? && self.fetched?)
+      @version = v
       puts "Skipping fetch of #{self.to_s}"
     else
       puts "Fetching the gem #{self.to_s}"
@@ -112,9 +127,9 @@ class RPGem
           gem_title = `ls`
           @version = gem_title.match(/-(\d+(\.\d+)+)\.gem$/)[1]
         end
-        system("mv #{TMP}/* #{sources_dir}/")
+        system("mv #{TMP}/* #{SOURCES_DIR}/")
       else
-        FileUtils.in_dir sources_dir do
+        FileUtils.in_dir SOURCES_DIR do
           system("gem fetch #{@name} --version #{@version}")
         end
       end
@@ -122,30 +137,47 @@ class RPGem
   end
 
   def make_spec!
-    #generate specfile
+    fetch!
+    puts "rendering the specfile..."
+    template = File.read("#{LIB_DIR}/template.spec.erb")
+    specfile = ERB.new(template).result(binding)
+    puts "rendered:"
+    puts "==============="
+    puts specfile
+    puts "==============="
+    File.write(spec_loc, specfile)
+    puts "rendered to #{spec_loc}"
   end
 
   def build!
-    #rpmbuild
-  end
+    fetch!
+    make_spec!
 
-  def recurse!(options={})
-    #TODO: add an option for dev dependencies
-    deps = self.spec.runtime_dependencies
-    deps.each do |d|
-      if !satisfied?(d)
-        self.class.new(d.name,
-          :version_reqs => d.requirements_list,
-          :recursive => true
-        ).setup!
-      end
+    puts "-*- Building RPM -*-"
+    FileUtils.in_dir RPMBUILD_DIR do
+      system("rpmbuild -vv -ba #{spec_loc}")
     end
+    puts "-*- Done Building RPM -*-"
   end
 
   def setup!
     fetch!
     make_spec!
     build!
-    recurse! if @recursive
+  end
+
+  def recurse!(action=:setup!)
+    #TODO: add an option for dev dependencies
+    deps = self.spec.runtime_dependencies
+    deps.each do |d|
+      if !fetched?(d)
+        g = self.class.new(d.name,
+          :version_reqs => d.requirements_list,
+          :recursive => true
+        )
+        g.send(action)
+        g.recurse!(action)
+      end
+    end
   end
 end
